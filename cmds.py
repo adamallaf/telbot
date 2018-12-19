5a6cba3035d07fe1d00561b80f68e4ca3d2299e6
Adam <darthmaul.8@hotmail.com>
2018-12-19 18:27:51 +0000

    Added typing chat action

--- cmds.py
+++ cmds.py
@@ -13,7 +13,9 @@ shell = {}
 
 
 def startCmd(bot, update, args):
-    bot.send_message(chat_id=update.message.chat_id, text="Hello!")
+    chat_id = update.message.chat_id
+    bot.send_chat_action(chat_id=chat_id, action='typing')
+    bot.send_message(chat_id=chat_id, text="Hello!")
 
 
 def armCmd(bot, update, args):
@@ -21,9 +23,11 @@ def armCmd(bot, update, args):
     user_id = update.message.from_user.id
     if not isAuthorized(user_id):
         return
+    chat_id = update.message.chat_id
+    bot.send_chat_action(chat_id=chat_id, action='typing')
     sys.stdout.write("{}: Arming!\n".format(user_id))
     alarm.arm()
-    bot.send_message(chat_id=update.message.chat_id, text="Armed!")
+    bot.send_message(chat_id=chat_id, text="Armed!")
 
 
 def disarmCmd(bot, update, args):
@@ -31,77 +35,89 @@ def disarmCmd(bot, update, args):
     user_id = update.message.from_user.id
     if not isAuthorized(user_id):
         return
+    chat_id = update.message.chat_id
+    bot.send_chat_action(chat_id=chat_id, action='typing')
     sys.stdout.write("{}: Disarming!\n".format(user_id))
     alarm.disarm()
-    bot.send_message(chat_id=update.message.chat_id, text="Disarmed!")
+    bot.send_message(chat_id=chat_id, text="Disarmed!")
 
 
 def addUserCmd(bot, update, args):
     user_id = update.message.from_user.id
+    chat_id = update.message.chat_id
     if not isAuthorized(user_id):
         return
     if not args:
-        bot.send_message(chat_id=update.message.chat_id, text="Wrong command use!\nusage: /adduser <id>")
+        bot.send_message(chat_id=chat_id, text="Wrong command use!\nusage: /adduser <id>")
         return
+    bot.send_chat_action(chat_id=chat_id, action='typing')
     _arg = int(args.pop(0))
     sys.stdout.write("{}\n".format("Adding user {} to white list...".format(_arg)))
     msg = "User {} already exists!".format(_arg)
     if addUser(_arg):
         msg = "User {} added!".format(_arg)
         sys.stdout.write("{}\n".format(msg))
-    bot.send_message(chat_id=update.message.chat_id, text=msg)
+    bot.send_message(chat_id=chat_id, text=msg)
 
 
 def removeUserCmd(bot, update, args):
     user_id = update.message.from_user.id
+    chat_id = update.message.chat_id
     if not isAuthorized(user_id):
         return
     if not args:
-        bot.send_message(chat_id=update.message.chat_id, text="Wrong command use!\nusage: /rmuser <id>")
+        bot.send_message(chat_id=chat_id, text="Wrong command use!\nusage: /rmuser <id>")
         return
+    bot.send_chat_action(chat_id=chat_id, action='typing')
     _arg = int(args.pop(0))
     sys.stdout.write("{}\n".format("Removing user {} from white list...".format(_arg)))
     msg = "User {} doesn\'t exists!".format(_arg)
     if removeUser(_arg):
         msg = "User {} removed!".format(_arg)
     sys.stdout.write("{}\n".format(msg))
-    bot.send_message(chat_id=update.message.chat_id, text=msg)
+    bot.send_message(chat_id=chat_id, text=msg)
 
 
 def getUsersCmd(bot, update, args):
     user_id = update.message.from_user.id
+    chat_id = update.message.chat_id
     if not isAuthorized(user_id):
         return
+    bot.send_chat_action(chat_id=chat_id, action='typing')
     sys.stdout.write("{}: get user list\n".format(user_id))
     user_list = getUsers()
     users = "{}".format("".join("{}\n".format(_u) for _u in user_list))
-    bot.send_message(chat_id=update.message.chat_id, text=users)
+    bot.send_message(chat_id=chat_id, text=users)
 
 
 def printUserID(bot, update, args):
     user_id = update.message.from_user.id
+    chat_id = update.message.chat_id
+    bot.send_chat_action(chat_id=chat_id, action='typing')
     sys.stdout.write("{}: what my ID\n".format(user_id))
-    bot.send_message(chat_id=update.message.chat_id, text=user_id)
+    bot.send_message(chat_id=chat_id, text=user_id)
 
 
 def printAvailableCmds(bot, update, args):
     user_id = update.message.from_user.id
+    chat_id = update.message.chat_id
     if not isAuthorized(user_id):
         return
+    bot.send_chat_action(chat_id=chat_id, action='typing')
     sys.stdout.write("{}: print available commands\n".format(user_id))
     cmds = "{}".format("".join(f"{_cmd}\n" for _cmd in CMDS.keys()))
-    bot.send_message(chat_id=update.message.chat_id, text=cmds)
+    bot.send_message(chat_id=chat_id, text=cmds)
 
 
 def execute(bot, update, args):
     global shell
     user_id = update.message.from_user.id
+    chat_id = update.message.chat_id
     if not isAuthorized(user_id):
         return
     if not args:
-        bot.send_message(chat_id=update.message.chat_id, text="Wrong command use!\nusage: /exec <cmd>")
+        bot.send_message(chat_id=chat_id, text="Wrong command use!\nusage: /exec <cmd>")
         return
-    chat_id = update.message.chat_id
     bot.send_chat_action(chat_id=chat_id, action='typing')
     _args = " ".join(args)
     sys.stdout.write("{}: execute {}\n".format(user_id, _args))
