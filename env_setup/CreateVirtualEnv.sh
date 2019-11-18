6a6e1b1091b7f493348d5daf6a75c727836c0c24
Adam Allaf <adam.allaf@gigaset.com>
2019-11-18 18:00:11 +0100

    Added properties for user base attributes

--- env_setup/CreateVirtualEnv.sh
+++ env_setup/CreateVirtualEnv.sh
@@ -13,7 +13,7 @@ ${PYTHON} -m pip install virtualenv
 (cd .pyEnv && ${PYTHON} -m virtualenv --python=${PYTHON} ${VIRTUAL_ENV})
 echo -e ${SHEBANG}"\nexport VIRTUAL_ENV=\"${VIRTUAL_ENV}\"\nsource .pyEnv/"${VIRTUAL_ENV}"/bin/activate\n" > ${ACTIVATION_SCRIPT}
 echo -e ${SHEBANG}"\ndeactivate\nunset VIRTUAL_ENV\n" > ${DEACTIVATION_SCRIPT}
-. ${ACTIVATION_SCRIPT}
+. ${ACTIVATION_SCRIPT} || { echo 'Virtual env activation failed' ; exit 1; }
 (cd env_setup && ${PYTHON} -m pip install -r requirements.txt --upgrade)
 
 . RunUnitTests.sh
--- user_base.py
+++ user_base.py
@@ -10,11 +10,26 @@ class UserPermissions(IntEnum):  # for now let it be like this
 class UserBase:
     def __init__(self, user_id, permissions):
         self.__user_id = user_id
+        self.__first_name = ""
+        self.__last_name = ""
+        self.__username = ""
         self.__user_permissions = permissions
 
     @property
     def id(self) -> int:
         return self.__user_id
+
+    @property
+    def fullname(self) -> str:
+        return self.__last_name
+
+    @property
+    def username(self) -> str:
+        return self.__username
+
+    @username.setter
+    def username(self, value):
+        self.__username = value
 
     @property
     def permissions(self) -> UserPermissions:
